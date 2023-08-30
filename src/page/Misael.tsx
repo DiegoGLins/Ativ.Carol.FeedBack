be463d5e7d6ccd09c7b3aed0d27a66609e08e969
Diego Lins <you@example.com>
2023-08-29 23:49:32 -0300

    criacao da pagina

--- src/page/Misael.tsx
+++ src/page/Misael.tsx
@@ -1,25 +1,97 @@
-// import * as C from '../App.styles'
-import { Grid } from '@mui/material';
-import { cardsArray } from '../components/Card/cardsArray';
-import CardSkill from '../components/Card/CardSkill';
+
+import { Container, Divider, Grid } from '@mui/material';
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 
-const Misael: React.FC = () => {
+const MisaelCardSkill: React.FC = () => {
+
     return (
-        <Grid md={5} xs={12} container item>
-            {
-                cardsArray.map((item) => (
-                    <CardSkill key={item.name}
-                        name={item.name}
-                        image={item.image}
-                        skill1={item.skill1}
-                        skill2={item.skill2}
-                        skill3={item.skill3}
-                    />
-                ))
-            }
-        </Grid>
-    )
-}
+        <>
+            <Container maxWidth='sm'>
+                <Grid xs={12} className='boxCard' container>
+                    <Grid container item>
+                        <img style={{ position: 'absolute', bottom: '90px', borderRadius: '40px', width: '180px', height: '230px', padding: '25px 15px 25px 30px' }} src={'./image/Misael.jpg'} alt='Misael'></img>
+                    </Grid>
+                    <Grid container item style={{
+                        marginTop: '15px',
+                        position: 'relative',
+                        width: '650px', padding: '30px 50px 118px 200px', justifyContent: 'flex-start', alignItems: 'flex-start', overflow: 'hidden'
+                    }}>
+                        <Grid item style={{
+                            fontFamily: 'sans-serif',
+                            fontSize: '24px',
+                            font: 'bold', textAlign: 'center',
+                            textTransform: 'uppercase'
+                        }}>
+                            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                                <p className='star'>{'\u2B50'}</p>
+                                <p>{'MISAEL'}</p>
+                            </div>
+                            <Divider style={{ border: '#fff solid 1px', marginTop: '10px', marginBottom: '20px', width: '300px' }} />
+                        </Grid>
+                        <Grid item style={{
+                            width: '100%',
+                            fontFamily: 'sans-serif',
+                            fontSize: '18px',
+                            fontStyle: 'normal', textAlign: 'start',
+                        }}>
 
-export default Misael;
+                            <Accordion style={{ marginBottom: '10px', color: '#fff', borderTopStyle: 'ridge', borderStartStartRadius: '30px', borderEndEndRadius: '30px', backgroundColor: 'transparent' }}>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon style={{ color: 'blue' }} />}
+                                    aria-controls="panel1a-content"
+                                    id="panel1a-header"
+                                >
+                                    <Typography >{'Paciente'}</Typography>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    <Typography>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                                        malesuada lacus ex, sit amet blandit leo lobortis eget.
+                                    </Typography>
+                                </AccordionDetails>
+                            </Accordion>
+                            <Accordion style={{ marginBottom: '10px', color: '#fff', borderTopStyle: 'groove', borderStartStartRadius: '30px', borderEndEndRadius: '30px', backgroundColor: 'transparent' }}>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon style={{ color: 'blue' }} />}
+                                    aria-controls="panel2a-content"
+                                    id="panel2a-header"
+                                >
+                                    <Typography>{'Organizado'}</Typography>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    <Typography>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                                        malesuada lacus ex, sit amet blandit leo lobortis eget.
+                                    </Typography>
+                                </AccordionDetails>
+                            </Accordion>
+                            <Accordion style={{ color: '#fff', borderTopStyle: 'groove', borderStartStartRadius: '30px', borderEndEndRadius: '30px', backgroundColor: 'transparent' }}>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon style={{ color: 'blue' }} />}
+                                    aria-controls="panel2a-content"
+                                    id="panel2a-header"
+                                >
+                                    <Typography>{'Inteligente'}</Typography>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    <Typography>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                                        malesuada lacus ex, sit amet blandit leo lobortis eget.
+                                    </Typography>
+                                </AccordionDetails>
+                            </Accordion>
+                        </Grid>
+                    </Grid >
+                </Grid>
+            </Container>
+        </>
+    );
+};
+
+export default MisaelCardSkill;
