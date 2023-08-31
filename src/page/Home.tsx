33468c8d903b9d2fde601e33742225788e2679c2
Diego Lins <you@example.com>
2023-08-31 15:20:01 -0300

    alteracao na Home

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,6 +1,5 @@
 
-// import { CardStyled } from './CardStyled';
-import { Container, Grid } from '@mui/material';
+// import { Box, Grid } from '@mui/material';
 import * as React from 'react';
 import NavBar from '../components/NavBar/NavBar';
 
@@ -9,14 +8,7 @@ const CardSkillHome: React.FC = () => {
 
     return (
         <>
-            <Container maxWidth='sm'>
-                <NavBar />
-                <Grid xs={12} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center' }} className='boxCard' container>
-                    <Grid md={6} xs={12} item>
-                        <img style={{ borderRadius: '100%', backgroundColor: '#fff', width: '250px', height: '250px' }} src={''}></img>
-                    </Grid>
-                </Grid>
-            </Container>
+            <NavBar />
         </>
     );
 };
