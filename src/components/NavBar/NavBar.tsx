bd0ea73812bd8025ee7c4e0cc1f540a51236616d
AlexandreS10 <alexandres10santos@gmail.com>
2023-08-31 13:17:10 -0300

    primeiro commit

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,33 +1,27 @@
-import { navigation } from '../../config/navigation';
-import { } from '../../index.css'
-import { Grid } from '@mui/material';
-import { Link } from 'react-router-dom';
-import { NavBarStlyed } from './NavBarStyled';
+import { navigation } from "../../config/navigation";
+import {} from "../../index.css";
+import { Grid } from "@mui/material";
+import { Link } from "react-router-dom";
+import { NavBarStlyed } from "./NavBarStyled";
 
-interface NavBarProps {
-  title: string;
-}
-
+interface NavBarProps {}
 
 const NavBar: React.FC<NavBarProps> = ({ title }) => {
-
   return (
     <>
       <Grid xs={12} container>
-        <NavBarStlyed >
+        <NavBarStlyed>
           <h1>{title}</h1>
-          <ul className='bodyNav'>
-            <li >
-              {
-                navigation.map((item => {
-                  return (
-                    <p key={item.key}>
-                      <Link to={item.url}>{item.label}</Link>
-                    </p>
-                  )
-                }
-                ))}
-              <div id='marker'></div>
+          <ul className="bodyNav">
+            <li>
+              {navigation.map((item) => {
+                return (
+                  <p key={item.key}>
+                    <Link to={item.url}>{item.label}</Link>
+                  </p>
+                );
+              })}
+              <div id="marker"></div>
             </li>
           </ul>
         </NavBarStlyed>
