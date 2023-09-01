e0dab87d645f0d2b790305a7b238e0dff0a2538a
Diego Lins <you@example.com>
2023-08-31 22:39:14 -0300

    refatoracao do main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import RoutesSkill from './routes/RoutesSkills'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RoutesSkill />
+  </React.StrictMode>
 )
