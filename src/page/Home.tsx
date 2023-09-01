6461fefcc81c9700c0a95739ff39b036bcb3e602
Diego Lins <you@example.com>
2023-08-31 22:41:17 -0300

    Home page em teste

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -2,6 +2,7 @@
 // import { Box, Grid } from '@mui/material';
 import * as React from 'react';
 import NavBar from '../components/NavBar/NavBar';
+import { Grid } from '@mui/material';
 
 
 const CardSkillHome: React.FC = () => {
@@ -9,6 +10,12 @@ const CardSkillHome: React.FC = () => {
     return (
         <>
             <NavBar />
+            <Grid xs={12} container item style={{ marginTop: '250px', position: 'relative', justifyContent: 'center', alignItems: 'center' }}>
+                <Grid className='boxCard' container item style={{ width: '100%', height: '220px', justifyContent: 'center', alignItems: 'center' }}>
+                    <img style={{ background: '#fff', position: 'absolute', bottom: '90px', borderRadius: '100%', width: '130px', height: '130px' }} src={''} ></img>
+                </Grid >
+            </Grid>
+
         </>
     );
 };
