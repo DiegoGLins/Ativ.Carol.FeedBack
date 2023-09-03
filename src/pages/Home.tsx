7aff8dad929d3ac36a13212a34c8c136367f35db
Diego Lins <you@example.com>
2023-09-02 23:13:21 -0300

    alteracao na Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,18 @@
+
 import React from "react";
 import Paragrafo from "../components/Paragrafo";
+
+
 
 const Home: React.FC = () => {
   return (
     <>
-      <Paragrafo titulo="Atividade React "></Paragrafo>
+
+      <div className='boxHome' >
+        <div className="boxCenter">
+          <Paragrafo conteudo={"Conheça as principais"} titulo={'BEM VINDO'} descricao={'Skills da nossa equipe'} />
+        </div>
+      </div>
     </>
   );
 };
