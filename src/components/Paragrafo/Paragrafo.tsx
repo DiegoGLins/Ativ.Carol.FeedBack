05f5ff014a01d6013b9d6c827bd3380e8a8e2b67
Diego Lins <you@example.com>
2023-09-02 23:10:44 -0300

    alteracao do componente paragrafo

--- src/components/Paragrafo/Paragrafo.tsx
+++ src/components/Paragrafo/Paragrafo.tsx
@@ -3,21 +3,20 @@ import React from "react";
 interface ParagrafoProps {
   titulo: string;
   conteudo?: string;
-  children?: React.ReactNode;
+  descricao: string;
 }
 
 const Paragrafo: React.FC<ParagrafoProps> = ({
   titulo,
   conteudo,
-  children,
+  descricao,
 }) => {
   return (
     <div
-      style={{ display: "Flex", flexDirection: "column", alignItems: "center" }}
-    >
-      <h1 style={{ color: "white", fontSize: "5rem", paddingTop: '70px' }}>{titulo}</h1>
-      <p>{conteudo}</p>
-      {children}
+      style={{ display: "Flex", flexDirection: "column", alignItems: "center" }}>
+      <h1 style={{ color: "white", fontSize: "5rem", fontFamily: 'sans-serif', padding: '30px 0px 0px' }}>{titulo}</h1>
+      <p className="content-P">{conteudo}</p>
+      <p style={{ color: 'white' }} className="descricao-P">{descricao}</p>
     </div>
   );
 };
